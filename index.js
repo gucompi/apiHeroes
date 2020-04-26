1a7040114712a45426daf5b129abf9dae84eaf86
gcompiano <gucompi@gmail.com>
2020-04-26 18:59:31 -0300

    Add pagination

--- index.js
+++ index.js
@@ -36,7 +36,17 @@ mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PSW}@${process
                 return res.json({err:"No se encontraron heroes."}).status(404)
                 return res.json(heroesFinded).status(200)
             }).catch((err)=>{
-                return res.json({err:err}).status(500)
+                return res.json({err:"Error al buscar heroes"}).status(500)
+            })
+    })
+    router.get('/all/',(req,res)=>{
+        Heroe.find({$or:[{"tokenAsociado":null},{"tokenAsociado":req.headers.token}]})
+            .then((heroesFinded)=>{
+                if(!heroesFinded)
+                return res.json({err:"No se encontraron heroes."}).status(404)
+                return res.json(heroesFinded).status(200)
+            }).catch((err)=>{
+                return res.json({err:"Error al buscar heroes"}).status(500)
             })
     })
 
